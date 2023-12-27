cae933694b5944ef804b25eba37c86a65476f57e
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-12-27 06:07:02 +0200

    fix(build): add @ alias to webpack

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
             },
         ],
     },
+    webpack: (config) => {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          '@': path.resolve(__dirname, './'),
+        };
+    
+        return config;
+      },
 };
 
 module.exports = nextConfig;
