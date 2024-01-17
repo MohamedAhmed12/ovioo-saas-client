60117b20b82f75c5f540a1ee2214b448b514600c
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-01-17 08:09:55 +0200

    fix(auth): fix registration bug with credentials provider

--- src/components/GetStartedBtns.tsx
+++ src/components/GetStartedBtns.tsx
@@ -1,13 +1,17 @@
 import "@/styles/components/get-started-btns.scss";
 import AnimatedBtn from "./AnimatedBtn";
+import Link from "next/link";
 
 export const GetStartedBtns = () => (
     <div className="get-started_btn-container">
-        <a href="/register" className="ghost-button-wrap new-cta_7for7 w-inline-block">
+        <Link
+            href="/auth/register"
+            className="ghost-button-wrap new-cta_7for7 w-inline-block"
+        >
             <div className="ghost-button new-cta">
                 <div className="buttons-l">Get started</div>
             </div>
-        </a>
+        </Link>
         <AnimatedBtn title="Book a Demo" />
     </div>
 );
