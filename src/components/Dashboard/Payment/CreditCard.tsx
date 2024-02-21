535ab75aec519c1f9adafa1d149d65bae2e20e11
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-21 07:55:11 +0200

    feat(payment): create credit card component

---
+++ src/components/Dashboard/Payment/CreditCard.tsx
@@ -0,0 +1,18 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+export default function CreditCard() {
+    return (
+        <Card className="ovioo-card with-shadow min-w-[300px] max-w-[350px] rounded-[10px] my-6">
+            <CardContent>
+                <Typography variant="h5" component="div" className="!text-[30px]">
+                    Credit
+                </Typography>
+                <Typography variant="h3" align="center" className="!my-5 !text-[45px]">
+                    $600
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
