7be606ccbdd9769078f50263b73fcd2f5d4b4b41
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-21 10:25:06 +0200

    feat(payment): add team credit to credit card component
    query  team credit implementation

--- src/components/Dashboard/Payment/CreditCard.tsx
+++ src/components/Dashboard/Payment/CreditCard.tsx
@@ -1,18 +1,60 @@
+import { authOptions } from "@/constants/authOptions";
+import { getClient } from "@/utils/getClient";
+import { ApolloClient, gql } from "@apollo/client";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { getServerSession } from "next-auth";
+import toast from "react-hot-toast";
 
-export default function CreditCard() {
+const FETCH_USER_WITH_PROFILE = gql`
+    query {
+        me {
+            teams {
+                subscriptions {
+                    remaining_credit_hours
+                }
+            }
+        }
+    }
+`;
+
+export default async function CreditCard() {
+    const session = await getServerSession(authOptions);
+    const client: ApolloClient<any> | undefined = getClient(session);
+
+    const { data, error } = await client.query({
+        query: FETCH_USER_WITH_PROFILE,
+    });
+
+    if (error) {
+        toast.error("Something went wrong!");
+    }
+
     return (
-        <Card className="ovioo-card with-shadow min-w-[300px] max-w-[350px] rounded-[10px] my-6">
-            <CardContent>
-                <Typography variant="h5" component="div" className="!text-[28px]">
-                    Credit
-                </Typography>
-                <Typography variant="h3" align="center" className="!my-5 !text-[45px]">
-                    $600
-                </Typography>
-            </CardContent>
-        </Card>
+        data && (
+            <Card className="ovioo-card with-shadow min-w-[300px] max-w-[350px] rounded-[10px] my-6">
+                <CardContent>
+                    <Typography
+                        variant="h5"
+                        component="div"
+                        className="!text-[28px]"
+                    >
+                        Credit
+                    </Typography>
+                    <Typography
+                        variant="h3"
+                        align="center"
+                        className="!my-5 !text-[45px]"
+                    >
+                        $
+                        {
+                            data.me.teams[0].subscriptions[0]
+                                .remaining_credit_hours
+                        }
+                    </Typography>
+                </CardContent>
+            </Card>
+        )
     );
 }
