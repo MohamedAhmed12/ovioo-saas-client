e543aebcb0eef6c286a5091d1687eeffb9c167c5
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-21 10:25:40 +0200

    feat(payment): query to get manage subs URL

--- src/components/Dashboard/Payment/SubscriptionCard.tsx
+++ src/components/Dashboard/Payment/SubscriptionCard.tsx
@@ -1,38 +1,75 @@
-import Button from "@mui/material/Button";
+import { authOptions } from "@/constants/authOptions";
+import { getClient } from "@/utils/getClient";
+import { ApolloClient, gql } from "@apollo/client";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const cardNumbers = 1234;
+const GET_MANAGE_SUBS_URL = gql`
+    query GetManageSubscriptionURL {
+        getManageSubscriptionURL
+    }
+`;
 
-export default function SubscriptionCard() {
+export default async function SubscriptionCard() {
+    const session = await getServerSession(authOptions);
+    const client: ApolloClient<any> | undefined = getClient(session);
+
+    const { data, error } = await client.query({
+        query: GET_MANAGE_SUBS_URL,
+    });
+
+    if (error) {
+        toast.error("Something went wrong!");
+    }
+
     return (
-        <Card
-            sx={{ minWidth: 275, maxWidth: 500, borderRadius: "10px" }}
-            className="ovioo-card with-shadow"
-        >
-            <CardContent>
-                <span className="flex justify-between">
-                    <Typography variant="h5" component="div" style={{ marginBottom: 32 }}>
-                        Current Plan <span className="capitalize text-slate-400">(pro)</span>
-                    </Typography>
+        data && (
+            <Card className="ovioo-card with-shadow min-w-[300px] max-w-[580px] rounded-[10px] my-6">
+                <CardContent>
+                    <span className="flex justify-between items-center mb-8">
+                        <Typography variant="h5" component="div">
+                            Current Plan
+                            <span className="capitalize text-slate-400 ml-1">
+                                (standard)
+                            </span>
+                        </Typography>
+                        <Link
+                            href={data.getManageSubscriptionURL}
+                            className="text-base text-sm text-slate-400 capitalize p-2 hidden md:flex text-wrap hover:text-white"
+                        >
+                            manage subscription
+                        </Link>
+                    </span>
+                    <span className="!text-5xl">
+                        <Typography style={{ marginBottom: 4 }} variant="h6">
+                            card number
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            **** **** **** {cardNumbers}
+                        </Typography>
+                    </span>
+                </CardContent>
+                <CardActions className="flex-col !items-start justify-start !p-4">
                     <Link
-                        href="/dashboard/plan"
-                        className="text-lg font-semibold text-slate-400 capitalize p-0"
+                        href={data.getManageSubscriptionURL}
+                        className="hover:bg-transparent dashboard__link !font-[800] !p-0"
                     >
-                        change plan
+                        Replace credit card
                     </Link>
-                </span>
-                <Typography style={{ marginBottom: 4 }}>card number</Typography>
-                <Typography variant="body2">**** **** **** {cardNumbers}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small" className="hover:bg-transparent dashboard__link" style={{ fontWeight: 700 }}>
-                    Replace credit card
-                </Button>
-            </CardActions>
-        </Card>
+                    <Link
+                        href={data.getManageSubscriptionURL}
+                        className="hover:bg-transparent dashboard__link !font-[800] !p-0 !pt-1 !m-0"
+                    >
+                        View Transactions
+                    </Link>
+                </CardActions>
+            </Card>
+        )
     );
 }
