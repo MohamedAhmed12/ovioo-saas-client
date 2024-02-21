0056f07b4b68e12024901cf644c4e54ae46baab6
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-21 09:33:39 +0200

    feat(stripe): add url for managing customer's subscription

--- src/components/Dashboard/Payment/CreditCard.tsx
+++ src/components/Dashboard/Payment/CreditCard.tsx
@@ -6,7 +6,7 @@ export default function CreditCard() {
     return (
         <Card className="ovioo-card with-shadow min-w-[300px] max-w-[350px] rounded-[10px] my-6">
             <CardContent>
-                <Typography variant="h5" component="div" className="!text-[30px]">
+                <Typography variant="h5" component="div" className="!text-[28px]">
                     Credit
                 </Typography>
                 <Typography variant="h3" align="center" className="!my-5 !text-[45px]">
